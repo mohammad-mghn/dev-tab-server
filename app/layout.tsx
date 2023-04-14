83758ad3101719b7f27c85a351fb667db1669744
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-04-14 17:23:10 +0330

    fix backend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-import "./globals.css";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
 
-      <body className="bg-primary overflow-x-hidden">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,11 @@
-import Header from "../components/header/header";
-import Navbar from "../components/navbar/navbar";
-import Footer from "../components/footer/footer";
-import Contact from "../components/contact/contact";
-import Features from "../components/features/features";
-import Statuses from "../components/statuses/statuses";
-import Broswers from "../components/broswers/broswers";
-import Redirect from "../components/redirect/redirect";
-import Testimonials from "../components/Testimonials/Testimonials";
-import SupportedWebsites from "../components/supportedWebsites/supportedWebsites";
-
 export default function Home() {
   return (
-    <div className="px-6 mx-auto lg:max-w-[55rem] 2xl:max-w-[75rem]">
-      <Navbar />
-
-      <main className="mt-[6rem] overflow-x-visible">
-        <Header />
-
-        <Features />
-
-        <Statuses />
-
-        <SupportedWebsites />
-
-        <Testimonials />
-
-        <Broswers />
-
-        <Contact />
-
-        <Redirect />
-
-        <Footer />
-      </main>
+    <div>
+      go to <code>/api/devto</code>
+      <br />
+      go to <code>/api/hackernews</code>
+      <br />
+      go to <code>/api/stackoverflow</code>
     </div>
   );
 }
